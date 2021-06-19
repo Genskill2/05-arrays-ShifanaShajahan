4a98f579adb3a63be49a729b9d9039b2f2602860
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-06-19 08:43:15 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,20 @@
-/* Enter your solutions in this file */
+/test_min.c/
 #include <stdio.h>
+int min(int[],int);
+int main(void)
+{
+int x[]={5};
+printf("%i",min(x,1));
+}
+int min(int a[],int n)
+{
+    int mi=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<mi)
+            mi=a[i];
+        else
+            continue;
+    }
+    return mi;
+}
